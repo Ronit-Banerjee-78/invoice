6938af6d638dd7563493ab8b06eb5cebceeb7d28
Ronit Banerjee <103939129+Ronit-Banerjee-78@users.noreply.github.com>
2025-02-04 18:09:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config({
 
 app.use(
   cors({
-    origin: 'https://invoice-six-theta.vercel.app/', //process.env.FRONT_END_URL,
+    origin: 'https://invoice-six-theta.vercel.app', //process.env.FRONT_END_URL,
     credentials: true,
   })
 );
